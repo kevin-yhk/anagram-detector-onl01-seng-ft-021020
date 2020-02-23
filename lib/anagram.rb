13fcc8acfad61386cc29c38a749d07f59e426719
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-02-23 00:23:15 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,12 +1,13 @@
 # Your code goes here!
 class Anagram   
   
-def initalize(word)
-  @word = word
-end 
+  def initalize(word)
+   @word = word
+  end 
 
-def match(array)
-  array.select do |words|
-    (@word.split("").sort) == (word.split("").sort)
-  end
+  def match(array)
+    array.select do |words|
+      (@word.split("").sort) == (word.split("").sort)
+    end
+  end 
 end 
