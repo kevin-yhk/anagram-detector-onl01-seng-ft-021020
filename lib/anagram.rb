473d71cd7d89824781be82add3d46cdec0ceed94
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-02-23 00:27:19 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,15 +1,14 @@
-# Your code goes here!
-class Anagram  
-  
-  attr_accessor :word
-  
-  def initalize(word)
-   @word = word
-  end 
+class Anagram
 
-  def match(array)
-    array.select do |words|
-      words.split("").sort == @word.split("").sort
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word_array)
+    word_array.select do |word|
+      word.split("").sort == @word.split("").sort
     end
-  end 
-end 
+  end
+end
